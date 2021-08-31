0e14f4bbb720df0d89d6a41929cef0f08e8c2551
Arno Schamber <arno.schamber@deutsche-boerse.com>
2021-08-31 12:06:08 +0200

    Adding some useful git aliases

--- git_config.sh
+++ git_config.sh
@@ -3,3 +3,10 @@
 git config --global user.name "Arno Schamber"
 git config --global user.email arno.schamber@deutsche-boerse.com
 git config --global init.defaultBranch main
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.unstage 'reset HEAD --'
+git config --global alias.last 'log -1 HEAD'
+git config --global alias.hist 'log --oneline --decorate --graph --all'
