d3e1ddb38f49bd3921eab5ae2208ef9e9f266cee
Arno Schamber <arno.schamber@deutsche-boerse.com>
2021-08-31 14:47:48 +0200

    Update compiler setup

--- compilers_config.sh
+++ compilers_config.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/bash
 # install rust to user environment
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+cargo --version 
+if [ $? -ne 0 ]
+then
+	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+fi
 
-# rm -rf /usr/local/go && tar -C /usr/local -xzf go1.17.linux-amd64.tar.gz # get the latest version of go
-# sudo dnf install golang-bin.x86_64 # install version 16 from official repo
+GO=/usr/local/go/bin/go 
+if [[ -x $GO ]] 
+then
+	$GO version	
+else
+	sudo cd /usr/local && wget https://golang.org/dl/go1.17.linux-amd64.tar.gz
+	sudo tar -C /usr/local -xzf go1.17.linux-amd64.tar.gz # get the latest version of go
+	#sudo dnf install golang-bin.x86_64 # install version 16 from official repo
+        /usr/local/go/bin/go install golang.org/x/tools/gopls@latest
+fi
