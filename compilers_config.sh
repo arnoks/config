624e684cb888c44459e34d093483a57d0f614928
Arno Schamber <arno.schamber@deutsche-boerse.com>
2021-09-01 09:51:49 +0200

    Caching go package after download
    The go archive is cached locally. Git will ignore
    the local file.

--- compilers_config.sh
+++ compilers_config.sh
@@ -3,16 +3,22 @@
 cargo --version 
 if [ $? -ne 0 ]
 then
+	echo "no rust version found, installing "
 	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 fi
 
-GO=/usr/local/go/bin/go 
-if [[ -x $GO ]] 
+# install go system wide
+GOPATH=/usr/local/go/bin/go 
+if [[ -x $GOPATH ]] 
 then
-	$GO version	
+	$GOPATH version	
 else
-	sudo cd /usr/local && wget https://golang.org/dl/go1.17.linux-amd64.tar.gz
-	sudo tar -C /usr/local -xzf go1.17.linux-amd64.tar.gz # get the latest version of go
-	#sudo dnf install golang-bin.x86_64 # install version 16 from official repo
+	echo "no go version found, installing "
+	GOVERSION=go1.17.linux-amd64.tar.gz
+	if [ ! -f $GOVERSION ] 
+	then
+		wget https://golang.org/dl/$GOVERSION
+	fi
+	sudo tar -C /usr/local -xzf $GOVERSION 
         /usr/local/go/bin/go install golang.org/x/tools/gopls@latest
 fi
