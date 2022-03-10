ba54a07932960a668fa9325ddd914531fffce982
Arno Schamber <arno.schamber@deutsche-boerse.com>
2022-03-10 13:58:02 +0100

    Adding some windows stuff

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ INITVIM=~/.config/nvim/init.vim
 
 all: ${INITVIM} ${TMUXCONF}
 
-~/.config/nvim/init.vim: init.vim
+~/.config/nvim/init.vim: init.vim lsrv.vim
 	cp  $< $@ 
 
 ~/.tmux.conf: tmux.conf 
---
+++ neovim.cmd
@@ -0,0 +1,13 @@
+
+# Install Chocolatey - requires elevated terminal session
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+# Install neovim
+
+choicolatey install neovim neovimgui
+
+# Install Plugin manager Plug from github
+iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim | ni "$(@($env:XDG_DATA_HOME, $env:LOCALAPPDATA)[$null -eq $env:XDG_DATA_HOME])/nvim-data/site/autoload/plug.vim" -Force
+
+
+# nvim -c PlugInstall
