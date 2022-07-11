465464b68fe99f380c8055374b2ea8e45cfb6a5f
Arno Schamber <arno.schamber@deutsche-boerse.com>
2022-07-11 13:40:50 +0200

    Updating Makefile

--- neovim_config.sh
+++ neovim_config.sh
@@ -2,9 +2,9 @@
 
 curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
 	       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-cp vimrc.viml "${XDG_DATA_HOME:-$HOME/.local/share}"/.config/nvim/nvim.init 
+cp init.vim "${XDG_DATA_HOME:-$HOME/.local/share}"/.config/nvim/nvim.init 
 nvim -u NONE -c PlugInstall -c -q 
 
 
 python3 -m pip install neovim
-sudo npm -g neovim pyright 
+sudo npm install -g neovim pyright 
