a3dca7e6ceb78d7da7c6b60a496cffc8ce74d7f9
Arno Schamber <arno.schamber@deutsche-boerse.com>
2022-08-22 13:32:40 +0200

    adding addiitional packages

--- user_config.sh
+++ user_config.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash 
 
 sudo dnf update -y
-sudo dnf install -y neovim fish make gcc g++ tmux fzf npm
+sudo dnf install -y neovim fish powerline-fonts make gcc g++ tmux fzf npm
 # allow remote log in by adding to fedora group
 sudo useradd -m -G wheel,fedora -c "Arno Schamber" -s /usr/bin/fish vn401 
 sudo mkdir ~vn401/.ssh
