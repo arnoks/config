b69f813096877c16a2cc5e875fd071aca1656541
Arno Schamber <arno.schamber@deutsche-boerse.com>
2022-08-23 19:02:16 +0200

    Update neovim_config.sh

--- neovim_config.sh
+++ neovim_config.sh
@@ -1,16 +1,16 @@
-#!/usr/bin/bash 
+#!/usr/bin/bash
 
 # Starting from Version 7. we can use lua based setup using packer
 
 if nvim --version | grep "NVIM v0.6"
 then
-	curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-		       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-	cp init.vim "${XDG_DATA_HOME:-$HOME/.local/share}"/.config/nvim/nvim.init 
-nvim -u NONE -c PlugInstall -c -q 
+    curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    cp init.vim "${XDG_DATA_HOME:-$HOME/.local/share}"/.config/nvim/nvim.init
+    nvim -u NONE -c PlugInstall -c -q
 else
     git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-    nvim -u NONE -c PackerInstall -c -q 
+    nvim -u NONE -c PackerInstall -c -q
 fi
 
 python3 -m pip install neovim
@@ -18,4 +18,4 @@ python3 -m pip install neovim
 #
 # install language server pyright
 
-sudo npm install -g neovim pyright 
+sudo npm install -g neovim pyright
